a96474f388e12595b09356152d53c1746569c614
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-10-29 13:30:09 +0300

    init

--- DJ_diplom/settings.py
+++ DJ_diplom/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'shop.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
-class MyUser(User):
+class User(AbstractUser):
     pass
 

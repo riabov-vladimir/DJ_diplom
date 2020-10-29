710aa81c52db9b72d0720620961efe4ebaf276c3
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-10-29 15:56:36 +0300

    models
    Product, Category, User

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Category, Product
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price']
+    list_editable = ['price']
+    prepopulated_fields = {'slug': ('name',)}
--- shop/models.py
+++ shop/models.py
@@ -5,3 +5,58 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+
+class Category(models.Model):
+
+    name = models.CharField(
+        max_length=50,
+        db_index=True
+    )
+
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(
+        Category,
+        related_name='products',
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(
+        max_length=70,
+        db_index=True
+    )
+
+    slug = models.SlugField(
+        max_length=70,
+        db_index=True
+    )
+
+    image = models.ImageField(
+        upload_to='products/%Y/%m/%d',
+        blank=True
+    )
+
+    description = models.TextField(blank=True)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name

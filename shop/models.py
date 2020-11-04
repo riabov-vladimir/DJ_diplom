09d8be1a46c0fae7226597fa8b9cd323256d420c
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-11-04 15:07:19 +0300

    urls, views added

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class User(AbstractUser):
@@ -25,6 +26,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_list_by_category',
+            args=[self.slug]
+        )
 
 
 class Product(models.Model):
@@ -60,3 +67,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_detail',
+            args=[self.id, self.slug]
+        )

0b8104bd99d5d7228d6e381550b0f5c075c1cbf7
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-11-11 22:59:47 +0300

    + cart urls
    migrations 003
    
    media fixed

--- DJ_diplom/urls.py
+++ DJ_diplom/urls.py
@@ -17,12 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from shop import views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
+    path('cart/', include('cart.urls', namespace='cart')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+
+from cart.forms import CartAddProductForm
 from shop.models import Category, Product, Article
 from django.views.generic import ListView
 
@@ -21,10 +23,33 @@ class IndexArticle(ListView):
 def catalog_view(request):
     pass
 
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
 
-def product_list(request):
-    pass
 
+def product_detail(request, id, slug):
+    # product = get_object_or_404(Product,
+    #                             id=id,
+    #                             slug=slug,
+    #                             available=True)
 
-def product_detail(request):
-    pass
+    product = Product.objects.get(id=id, slug=slug)
+
+    cart_product_form = CartAddProductForm()
+
+    print(product.image)
+
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
